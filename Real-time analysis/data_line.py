19322a5f18a30be096e2b15aa5d9279f140c71cc
AlcetA <demorait@gmail.com>
2021-03-30 21:32:19 +0300

    Update data_line.py by Jim
    Μικρη διορθωση στη συγκεντρωση CO2, NRcomp, Scomp και συγκεντρωση Ο3

--- Real-time analysis/data_line.py
+++ Real-time analysis/data_line.py
@@ -27,21 +27,44 @@ class data_line:
         T_in = self.df.T_in
         
         a= 1.52
-        n= 0.724 # linearization coefficients υπολογιστουν στα tests
+        n= 0.724 # linearization coefficients υπολογιστουν στα tests (me fittings)
+        Z = 1.33 
+        S = 0.3604   # Zero και Span , θα υπολογιστούν οταν γίνουν τα test (me fittings)
+        Tcal = 19.78 #kata protimhsh 20
+        apos = 0.000556
+        aneg = 0.000438
+        bpos = 0.124
+        bneg = 0.219
         
-        # I somehow calculate NRcomp & Scomp
-        NRcomp=0
-        Scomp=0
-          
-        Conc= ((-1/a)*np.ln(1-NRcomp/Scomp)) **(1/n)
-        return Conc
+        NR = CO2_V1/(Z*CO2_V2)
+        
+        if T_in > Tcal:
+            NRcomp = NR*(1+apos*(T_in-Tcal))
+            Scomp = S + bpos*(T_in-Tcal)/Tcal
+        else:
+            NRcomp = NR*(1+neg*(T_in-Tcal))
+            Scomp = S + bneg*(T_in-Tcal)/Tcal
+        
+        if 1 - NRcomp > 0:
+            Conc_CO2= ((-1/a)*np.ln(1-(1-NRcomp)/Scomp)) **(1/n)
+        else:
+            Conc_CO2= -((-1/a)*np.ln((1-(1-NRcomp)*(-1)/Scomp)) **(1/n)  ## το ειδα στις οδηγιες, αν δε το θυμηθω πες να το συζητησουμε
+        
+        return Conc_CO2 #giati error?
     
     @property
     def O3Concetration(self):
         
-        O3_V1 = self.df.O3_V1
-        O3_V2 = self.df.O3_V2
-        T_in = self.df.T_in
+        O3_WE = self.df.O3_WE ## νομιζω πρεπει να φτιαχτουν
+        O3_AE = self.df.O3_AE
+        T_in = self.df.T_in  # σημαντικη για τις τιμες που θα χρησιμοποιοηθουν στις σταθερες
         
-        # Calculating O3
-        pass
+        a = 1.64 # correction factor, εξαρταται απο θερμοκρασια μετρησης και σχετιζεται με τις επιφανειες των 2 μετρητων; 
+        WEe = 37
+        AEe = 25 # μετριουνται σε nanoAmpere , ενδεικτικες τιμες απο τον πινακα για θερμοκρασιες ~20
+        b = 1 #η σταθερα που συνδεει συγκεντρωση και διορθωμενη μετρηση ρευματος, δεν ξερω ενδεικτικες τιμες, υπολογιζεται στο calibration
+        WEc = Ο3_WE - WEe - a*(Ο3_AE -AEe)
+        
+        
+        Conc_O3 = b * WEc
+        return Conc_O3
