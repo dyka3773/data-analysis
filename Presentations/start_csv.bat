bdf926d924e24f5e642e5c0cfa3fad059ae4189c
dyka3773 <63119280+dyka3773@users.noreply.github.com>
2021-05-04 13:34:01 +0300

    Preparing for presentation

--- Presentations/start_csv.bat
+++ Presentations/start_csv.bat
@@ -1,7 +1,7 @@
 @echo off
 call C:\ProgramData\Anaconda3\Scripts\activate.bat
 
-cd C:/Users/Herck/GitHub Repos/data-analysis-ecowise/Presentations/
-python csv_sim.py
+:: cd C:/Users/Herck/GitHub Repos/data-analysis-ecowise/Presentations/
+python ./csv_sim.py
 
 pause
