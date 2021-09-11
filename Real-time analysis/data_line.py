b946b654e60a06ef794f1cbd27a4edd9c30814c7
AlcetA <demorait@gmail.com>
2021-09-11 20:38:48 +0300

    Swapped fake data for real. Changed some constants
    Swapped fake data for real. Changed some constants

--- Real-time analysis/data_line.py
+++ Real-time analysis/data_line.py
@@ -12,8 +12,9 @@ def O3ConcentrationA(df):
     O3_AE = df.O3_AE_a
     T_in = df.T_in  # σημαντικη για τις τιμες που θα χρησιμοποιηθουν στις σταθερες
     
+    a = 2
     # a = 1.3 # correction factor, εξαρταται απο θερμοκρασια μετρησης και σχετιζεται με τις επιφανειες των 2 μετρητων; 
-    a = -4e-11*T_in**6 + 6e-9*T_in**5 + 2e-7*T_in**4 - 1e-5*T_in**3 - 2e-6*T_in**2 + 0.0253*T_in + 1.2669
+    #a = -4e-11*T_in**6 + 6e-9*T_in**5 + 2e-7*T_in**4 - 1e-5*T_in**3 - 2e-6*T_in**2 + 0.0253*T_in + 1.2669
     # Πολυωνυμικό fitting απο το Excel, πιό μεγάλο R^2 απο το εκθετικό (0,9995), προέκυψε απο τον πίνακα με τις τιμές στο SED 
     WEe = 37
     AEe = 25 # μετριουνται σε nanoAmpere , ενδεικτικες τιμες απο τον πινακα για θερμοκρασιες ~20
@@ -29,7 +30,8 @@ def O3ConcentrationB(df):
     T_in = df.T_in  # σημαντικη για τις τιμες που θα χρησιμοποιηθουν στις σταθερες
     
     # a = 1.3 # correction factor, εξαρταται απο θερμοκρασια μετρησης και σχετιζεται με τις επιφανειες των 2 μετρητων; 
-    a = -4e-11*T_in**6 + 6e-9*T_in**5 + 2e-7*T_in**4 - 1e-5*T_in**3 - 2e-6*T_in**2 + 0.0253*T_in + 1.2669
+    #a = -4e-11*T_in**6 + 6e-9*T_in**5 + 2e-7*T_in**4 - 1e-5*T_in**3 - 2e-6*T_in**2 + 0.0253*T_in + 1.2669
+    a = 2
     WEe = 37
     AEe = 25 # μετριουνται σε nanoAmpere , ενδεικτικες τιμες απο τον πινακα για θερμοκρασιες ~20
     b = 1 # η σταθερα που συνδεει συγκεντρωση και διορθωμενη μετρηση ρευματος, δεν ξερω ενδεικτικες τιμες, υπολογιζεται στο calibration
@@ -43,8 +45,8 @@ def CO2ConcentrationA(df):
     CO2_V2 = df.CO2_V2_a
     T_in = df.T_in
     
-    a= 0.652
-    n= 0.746 # linearization coefficients υπολογιστουν στα tests (me fittings)
+    a= 2.49
+    n= 0.811 # linearization coefficients υπολογιστουν στα tests (me fittings)
     Z = 1.33 
     S = 0.4408   # Zero και Span , θα υπολογιστούν οταν γίνουν τα test (me fittings)
     Tcal = 19.78 # kata protimhsh 20
@@ -75,8 +77,8 @@ def CO2ConcentrationB(df):
     CO2_V2 = df.CO2_V2_b
     T_in = df.T_in
     
-    a= 0.652
-    n= 0.746 # linearization coefficients υπολογιστουν στα tests (me fittings)
+    a= 2.49
+    n= 0.811 # linearization coefficients υπολογιστουν στα tests (me fittings)
     Z = 1.33 
     S = 0.4408   # Zero και Span , θα υπολογιστούν οταν γίνουν τα test (me fittings)
     Tcal = 19.78 # kata protimhsh 20
