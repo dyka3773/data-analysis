58c7c3345cbe3f9475afc12bc958319aab7e1767
Hercules Konsoulas <dyka3773@gmail.com>
2021-11-21 13:17:24 +0000

    Update transformations.py

--- Post analysis/transformations.py
+++ Post analysis/transformations.py
@@ -1,5 +1,29 @@
 import pandas as pd
+import csv
 
-tsv1 = pd.read_csv("New Text Document.txt", sep='\t')
+tsv1 = pd.read_csv("testing.csv")
 
-tsv1.to_excel("BX30_Esrange_20210930_045350_EDT_PTULevels.xlsx")
+# print(tsv1)
+
+# list =[]
+
+# for line in tsv1.loc[:,0]:
+#     list.append(line.split())
+
+
+# # print(list)
+
+# with open('testing.csv', 'w', newline='') as f:
+      
+#     # using csv.writer method from CSV package
+#     write = csv.writer(f)
+
+#     string = " min  s      hPa      gpm     deg_C      hum  deg_C  Automatic  Operator  flag1   flag2"
+
+#     write.writerow(string.split())
+
+#     for listItem in list:
+#         write.writerow(listItem)
+    
+
+tsv1.to_excel("BX31_Esrange_20210929_071127_EDT_PTULevels.xlsx")
